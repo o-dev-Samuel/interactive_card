c8a7e38a8da66b280221f7d75d13a50d2c0a97c3
o-dev-Samuel <samuel.xw12@gmail.com>
2022-08-21 19:40:22 -0300

    Adicionei label e placeholder

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var cardnameinput = window.document.getElementById('cardnameinput')
+
+var cardnumberinput = window.document.getElementById('cardnumberinput')
+
+var cardmesinput = window.document.getElementById('cardmesinput')
+
+var cardanoinput = window.document.getElementById('cardanoinput')
+
+var cardcodinput = window.document.getElementById('cardcodinput')
+
+var cardname = document.getElementById('cardname')
+
+
+
+
+
+    
